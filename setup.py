3b517f88b0ce53bfb563d7573511396bdbd98031
Akita Kaito <kaitoakita07071@gmail.com>
2018-04-20 23:29:47 +0900

    license add

--- setup.py
+++ setup.py
@@ -30,5 +30,6 @@ setup(
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 )
